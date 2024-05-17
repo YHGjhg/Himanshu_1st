6362555cca4f9e9fda90862a7ab8f539efd10057
Himanshu <himanshushawdgp@gmail.com>
2024-05-17 20:55:32 +0530

    This is feature 1

--- FirstJavaCode.java
+++ FirstJavaCode.java
@@ -1,5 +1,9 @@
 public class FirstJavaCode{
     public static void main(String args[]){
+        int arr[] = {1, 4, 2, 7};
         System.out.println("My name is Himanshu Shaw !!!!");
+        for(int y : arr){
+            System.out.print(y+" ");
+        }
     }
 }
